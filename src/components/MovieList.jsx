cee3e5bbc85a960641a767685663a8f2995962ed
Gabriel Dias <gabriel.menezes.dias@hotmail.com>
2021-04-28 16:25:55 -0300

    request 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,22 @@
-// implement MovieList component here
+import React from 'react';
+import '../App.css';
+import MovieCard from './MovieCard';
+import PropTypes from 'prop-types';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    console.log(movies); 
+    return (
+      <section>
+        <MovieCard />
+      </section>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default MovieList;
