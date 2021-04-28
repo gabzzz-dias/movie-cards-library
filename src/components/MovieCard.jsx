9d6049b63648edd598525882002e57821fd1b484
Gabriel Dias <gabriel.menezes.dias@hotmail.com>
2021-04-28 16:52:45 -0300

    lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
     return (
       <section>
+        <p>aaa</p>
       </section>
-    )
+    );
   }
 }
 
