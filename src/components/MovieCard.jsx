0e24864e9788c0d5ff654cbaaa0ccad2f3086a84
Gabriel Dias <gabriel.menezes.dias@hotmail.com>
2021-04-30 01:03:03 -0300

    so falta 2

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
     const { title, subtitle, storyline, rating, imagePath } = movie;
     return (
-      <div>
-        <section>
-          <img src={ imagePath } alt={ title } />
-          <h4>{ title }</h4>
-          <h5>{ subtitle }</h5>
-          <p>{ storyline }</p>
-        </section>
-        <section>
-          { rating }
-        </section>
-      </div>
+      <section>
+        <img src={ imagePath } alt={ title } />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <Rating rating={ rating } />
+      </section>
     );
   }
 }
